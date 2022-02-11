086fc58e49544851e2ed80b0dc080d609cbfdcaf
Michael Reinig <michaelreinigjr@gmail.com>
2022-02-11 17:44:05 -0600

    fully retrieves a Github org app access token

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use serde::{Serialize, Deserialize};
-use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey};
+use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
 use chrono::Utc;
 use std::fs;
+use serde_json;
+use clap::Parser;
 
-use clap::{Parser, Subcommand};
+static APP_USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+);
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -15,9 +21,13 @@ struct Args {
     /// Relative path to the Github App private key.
     #[clap(short, long)]
     private_key_path: String,
+
+    /// Organization name as it appears in the github url, i.e. https://github.com/my-org/my-repo.
+    #[clap(short, long)]
+    org: String,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
@@ -26,6 +36,26 @@ fn main() {
         iat: usize,
         exp: usize,
         iss: String,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct GhInstallationResponse {
+        id: u32,
+        access_tokens_url: String,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct GhAccessTokenResponse {
+        token: String,
+        expires_at: String,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    struct FinalOutput {
+        access_token: String,
+        expiration: String,
+        access_token_url: String,
+        installation_id: u32,
     }
 
     let issued = Utc::now()
@@ -48,5 +78,33 @@ fn main() {
 
     let token = encode(&Header::new(Algorithm::RS256), &claims, &EncodingKey::from_rsa_pem(&private_key).unwrap()).unwrap();
 
-    println!("{}", token);
+    let client = reqwest::blocking::Client::new();
+
+    let res = client.get(format!("https://api.github.com/orgs/{}/installation", args.org))
+        .header("Authorization", format!("Bearer {}", token))
+        .header("User-Agent", APP_USER_AGENT)
+        .send()?
+        .text()?;
+
+    let gh_installation_response: GhInstallationResponse = serde_json::from_str(&res)?;
+    let access_token_url = &gh_installation_response.access_tokens_url;
+
+    let res = client.post(access_token_url)
+        .header("Authorization", format!("Bearer {}", token))
+        .header("User-Agent", APP_USER_AGENT)
+        .send()?
+        .text()?;
+
+    let gh_access_token_response: GhAccessTokenResponse = serde_json::from_str(&res)?;
+
+    let output = FinalOutput {
+        access_token: gh_access_token_response.token,
+        expiration: gh_access_token_response.expires_at,
+        access_token_url: gh_installation_response.access_tokens_url.to_string(),
+        installation_id: gh_installation_response.id,
+    };
+
+    println!("{}", serde_json::to_string(&output)?);
+
+    Ok(())
 }
