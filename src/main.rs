9c8f578560ebf46a7f5e1481df67ed97dcd730f6
Michael Reinig <michaelreinigjr@gmail.com>
2022-02-14 12:23:07 -0600

    cargo fmt and some restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use serde::{Serialize, Deserialize};
-use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
 use chrono::Utc;
-use std::fs;
-use serde_json;
 use clap::Parser;
+use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs;
 
-static APP_USER_AGENT: &str = concat!(
-    env!("CARGO_PKG_NAME"),
-    "/",
-    env!("CARGO_PKG_VERSION"),
-);
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -27,36 +23,36 @@ struct Args {
     org: String,
 }
 
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    iat: usize,
+    exp: usize,
+    iss: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct GhInstallationResponse {
+    id: u32,
+    access_tokens_url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct GhAccessTokenResponse {
+    token: String,
+    expires_at: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct FinalOutput {
+    access_token: String,
+    expiration: String,
+    access_token_url: String,
+    installation_id: u32,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-
-    /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
-    #[derive(Debug, Serialize, Deserialize)]
-    struct Claims {
-        iat: usize,
-        exp: usize,
-        iss: String,
-    }
-
-    #[derive(Debug, Deserialize, Serialize)]
-    struct GhInstallationResponse {
-        id: u32,
-        access_tokens_url: String,
-    }
-
-    #[derive(Debug, Deserialize, Serialize)]
-    struct GhAccessTokenResponse {
-        token: String,
-        expires_at: String,
-    }
-
-    #[derive(Debug, Deserialize, Serialize)]
-    struct FinalOutput {
-        access_token: String,
-        expiration: String,
-        access_token_url: String,
-        installation_id: u32,
-    }
 
     let issued = Utc::now()
         .checked_add_signed(chrono::Duration::seconds(-10))
@@ -67,20 +63,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("valid timestamp")
         .timestamp();
 
-
     let claims = Claims {
-            iat: issued.timestamp() as usize,
-            exp: expiration as usize,
-            iss: args.app_id.to_string(),
+        iat: issued.timestamp() as usize,
+        exp: expiration as usize,
+        iss: args.app_id.to_string(),
     };
 
     let private_key = fs::read(args.private_key_path).unwrap();
 
-    let token = encode(&Header::new(Algorithm::RS256), &claims, &EncodingKey::from_rsa_pem(&private_key).unwrap()).unwrap();
+    let token = encode(
+        &Header::new(Algorithm::RS256),
+        &claims,
+        &EncodingKey::from_rsa_pem(&private_key).unwrap(),
+    )
+    .unwrap();
 
     let client = reqwest::blocking::Client::new();
 
-    let res = client.get(format!("https://api.github.com/orgs/{}/installation", args.org))
+    let res = client
+        .get(format!(
+            "https://api.github.com/orgs/{}/installation",
+            args.org
+        ))
         .header("Authorization", format!("Bearer {}", token))
         .header("User-Agent", APP_USER_AGENT)
         .send()?
@@ -89,7 +93,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let gh_installation_response: GhInstallationResponse = serde_json::from_str(&res)?;
     let access_token_url = &gh_installation_response.access_tokens_url;
 
-    let res = client.post(access_token_url)
+    let res = client
+        .post(access_token_url)
         .header("Authorization", format!("Bearer {}", token))
         .header("User-Agent", APP_USER_AGENT)
         .send()?
